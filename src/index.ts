71a4bbd330325a3bd6f23de87b8e9101417a7b1e
lyukio <lyukio@hotmail.com>
2019-11-18 18:19:41 -0300

    Adding req and res on method

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
+import { Request, Response } from "express"
+
 export class Teste {
     id = "testeId"
     name = "nameId"
 
-    async fazNada() {
+    async route_fazNadaJSON(req: Request, res: Response) {
         console.log("fez nada. Dahora");
         console.log("this.id:")
         console.log(this.id)
         console.log("this.name:")
         console.log(this.name)
+        return res.json({"response": "Não fez nada. Deu certo"})
     }
 }
 
